6d91e7b815e5962378b25b7d4f6ee374de271aff
GustavoDasa <104524315+GustavoDasa@users.noreply.github.com>
2022-10-22 03:25:15 +0000

    aprimoramento de combate

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 from colorama import Fore
-import os, random
+import os
 
-e = {1:1,2:2,3:3,4:10,5:20,6:30,7:100,8:200,9:300}
-eb = {1:1,2:2,3:3,10:4,20:5,30:6,100:7,200:8,300:9}
-
-k = {6:[3,4,5],60:[30,40,50],600:[300,400,500],111:[11,101,110],222:[22,202,220],333:[33,303,330],123:[120,103,23],321:[21,301,320]}
-
+wins=[[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[7,5,3]]
 
 def Humano():
-    global j, possiveis, jogador, jog,p
-    print(f'\n {j[1]} | {j[2]} | {j[3]}\n---+---+---\n {j[4]} | {j[5]} | {j[6]}\n---+---+---\n {j[7]} | {j[8]} | {j[9]}\n')
+    global jogo, possiveis, jogador,todo
+    Board()
     while True:
         try:
-            x = int(input('Insira a casa que deseja jogar: '))
+            x = int(input('Jogador ' +Fore.BLUE + "X" + Fore.RESET+'\nInsira a casa que deseja jogar: '))
             if x in possiveis:
-                j[x] = Fore.BLUE + "X" + Fore.RESET
-                p[x] = -1
+                jogo[x] = Fore.BLUE + "X" + Fore.RESET
+                todo[x] = -1
                 jogador.append(x)
-                jog.append(e[x])
                 possiveis[x - 1] = 0
+                poss.remove(x)
                 Perdeu()
             else:
                 continue
@@ -26,127 +22,124 @@ def Humano():
         except:
             print('\nInsira um valor válido\n')
 
+def Humano2():
+    global jogo, possiveis, jogador,todo
+    Board()
+    while True:
+        try:
+            x = int(input('Jogador '+Fore.RED + "O" + Fore.RESET + '\nInsira a casa que deseja jogar: '))
+            if x in possiveis:
+                jogo[x] = Fore.RED + "O" + Fore.RESET
+                todo[x] = 1
+                maquina.append(x)
+                possiveis[x - 1] = 0
+                poss.remove(x)
+                Perdeu()
+            else:
+                continue
+            break
+        except:
+            print('\nInsira um valor válido\n')
 
 def Perdeu():
-    global g
-
-    teta = f'\n {j[1]} | {j[2]} | {j[3]}\n---+---+---\n {j[4]} | {j[5]} | {j[6]}\n---+---+---\n {j[7]} | {j[8]} | {j[9]}\n'
+    global g,vench
+    winner = {-3:jogador,3:maquina}
     ganhador = {-3: Fore.BLUE + "\nVocê venceu!!" + Fore.RESET ,3: Fore.RED +"\nVocê perdeu!!"+Fore.RESET}
     for i in [-3,3]:
-        if sum([p[1],p[2],p[3]]) == i:
-            print(ganhador[i])
-            g = True
-            print(teta)
-            break
-        elif sum([p[4],p[5],p[6]]) == i:
-            print(ganhador[i])
-            g = True
-            print(teta)
-            break
-        elif sum([p[7],p[8],p[9]]) == i:
-            print(ganhador[i])
-            g = True
-            print(teta)
-            break
-        elif sum([p[1],p[4],p[7]]) == i:
-            print(ganhador[i])
-            g = True
-            print(teta)
-            break
-        elif sum([p[2],p[5],p[8]]) == i:
-            print(ganhador[i])
-            g = True
-            print(teta)
-            break
-        elif sum([p[3],p[6],p[9]]) == i:
-            print(ganhador[i])
-            g = True
-            print(teta)
-            break
-        elif sum([p[1],p[5],p[9]]) == i:
-            print(ganhador[i])
-            g = True
-            print(teta)
-            break
-        elif sum([p[7],p[5],p[3]]) == i:
-            print(ganhador[i])
-            g = True
-            print(teta)
+        for winn in wins:
+            if len(set(winn) & set(winner[i]))==3:
+                print(ganhador[i])
+                vench = ganhador[i]
+                g = True
+                Board()
+                break
+        if g == True:
             break
     if sum(possiveis)==0 and g== False:
         g = True
         print(Fore.YELLOW + '\nDeu Velha' + Fore.RESET)
-        print(teta)
+        Board()
 
+def Bloq():
+    global lance,o
+
+  # Ataca pra vencer
+    for i in wins:
+      for j in possiveis:
+          if len(set(i) & set([j] + maquina)) == 3:
+              o = j
+              lance = True
+              break
+      if lance == True:
+          break
+
+  # Bloqueia oponente
+    if lance == False:
+      for i in wins:
+        for j in possiveis:
+          if len(set(i) & set([j]+jogador))==3:
+            o = j
+            lance = True
+            break
+        if lance==True:
+          break
 
 def Bordas():
-    global j, r, possiveis, maq, maquina, o,p
-
+    global jogo, possiveis, maquina,todo,o
     for i in [1, 3, 7, 9]:
         if i in possiveis:
             o = i
-            maq.append(e[i])
             maquina.append(i)
             possiveis[i - 1] = 0
-            j[i] = Fore.RED + "O" + Fore.RESET
-            r[i] = 1
-            p[i] = 1
+            jogo[i] = Fore.RED + "O" + Fore.RESET
+            todo[i] = 1
+            poss.remove(o)
             break
 
-
 def Robo():
-    global j, r, possiveis, maq, maquina, o,p,g
+    global jogo, robo, possiveis, maquina,todo,g,lance,o
+
     if g == False:
         lance = False
-        if len(jogador) == 1:
-            Bordas()
+        Bloq()
+        if lance == True:
+            maquina.append(o)
+            possiveis[o - 1] = 0
+            jogo[o] = Fore.RED + "O" + Fore.RESET
+            todo[o] = 1
+            poss.remove(o)
         else:
-            if len(jogador)>2:
-                jog.pop(0)
-            for i in k:
-                if sum(jog) in k[i] and possiveis[eb[i - sum(jog)] - 1] != 0:
-                    o = eb[i - sum(jog)]
-                    maq.append(e[o])
-                    maquina.append(o)
-                    possiveis[o - 1] = 0
-                    j[o] = Fore.RED + "O" + Fore.RESET
-                    r[o] = 1
-                    p[o] = 1
+            for i in wins:
+                if len(set(i) & set(poss + [o])) == 3:
+                    alpha = list(set(i)-set([o]))
+                    o = alpha[0]
                     lance = True
                     break
-            if lance == False:
-                poss=[]
-                for i in possiveis:
-                    if i!=0:
-                        poss.append(i)
-                o = random.choice(poss)
-                maq.append(e[o])
-                maquina.append(o)
-                possiveis[o - 1] = 0
-                j[o] = Fore.RED + "O" + Fore.RESET
-                r[o] = 1
-                p[o] = 1
+            if lance==False:
+                o = poss[0]
+            maquina.append(o)
+            possiveis[o - 1] = 0
+            jogo[o] = Fore.RED + "O" + Fore.RESET
+            todo[o] = 1
+            poss.remove(o)
         Perdeu()
 
-
-
-
 def Jogo1():
+    global o
     'Começa o humano, se jogar no meio, robo joga na bordo, e vice-versa'
     Humano()
     if 5 in jogador:
         Bordas()
     else:
-        maquina.append(5)
-        maq.append(20)
-        possiveis[5 - 1] = 0
-        j[5] = Fore.RED + "O" + Fore.RESET
-        r[5] = 1
-        p[5] = 1
+        o = 5
+        maquina.append(o)
+        possiveis[o - 1] = 0
+        jogo[o] = Fore.RED + "O" + Fore.RESET
+        todo[o] = 1
+        poss.remove(o)
     while g == False:
         Humano()
         Robo()
-
 
 def Jogo2():
     'Começa robo, joga nas bordas '
@@ -155,30 +148,40 @@ def Jogo2():
         Humano()
         Robo()
 
+def Jogo3():
+    'Começa jogador 1 vs jogador 2'
+    while g == False:
+        Humano()
+        if g == True:
+          break
+        Humano2()
+
+def Board():
+  print(f'\n {jogo[1]} | {jogo[2]} | {jogo[3]}\n---+---+---\n {jogo[4]} | {jogo[5]} | {jogo[6]}\n---+---+---\n {jogo[7]} | {jogo[8]} | {jogo[9]}\n')
 
 
 while True:
     try:
         # os.system("cls")
-        j = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9}
-        r = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
-        p = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
+        poss = [1,2,3,4,5,6,7,8,9]
+        jogo = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9}
+        todo = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
         possiveis = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         jogador = []
-        jog = []
         maquina = []
-        maq = []
         g = False
-
-        if int(input('\n1 - Você começa:\n2 - Robô começa:\n')) == 1:
+        y = int(input('\n1 - Você vs Máquina:\n2 - Máquina vs Você:\n3 - Você vs Outro:\n'))
+        if y == 1:
           Jogo1()
-        else:
+        elif y==2:
           Jogo2()
+        elif y ==3:
+          Jogo3()
         input('\nTecle ENTER para continuar')
     except:
         print('\nInsira um valor válido!\n')
 
 
 # Executa o programa
-if __name__ == "__main__":
-	main()
+# if __name__ == "__main__":
+# 	main()
